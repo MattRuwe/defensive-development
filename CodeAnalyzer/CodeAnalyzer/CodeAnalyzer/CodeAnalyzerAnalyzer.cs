90bcdd75dc101e5d490ba422634f25a4f3833a03
Matt Ruwe <matt.ruwe@kiewit.com>
2020-01-30 09:09:39 -0600

    Cleaned up the code a bit.

--- CodeAnalyzer/CodeAnalyzer/CodeAnalyzer/CodeAnalyzerAnalyzer.cs
+++ CodeAnalyzer/CodeAnalyzer/CodeAnalyzer/CodeAnalyzerAnalyzer.cs
@@ -15,8 +15,6 @@ namespace CodeAnalyzer
     {
         public const string DiagnosticId = "CodeAnalyzer";
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
         private static readonly string Title = "If statement nesting level is too high";
         private static readonly string MessageFormat = "The if statement nesting level is too high in method {0}";
         private static readonly string Description = "The if statement nesting level is too high which leads to code that is difficult to maintain.  Consider implementing a method to reduce the nesting level.";
@@ -35,7 +33,7 @@ namespace CodeAnalyzer
         {
             var ifStatementSyntax = (IfStatementSyntax)context.Node;
 
-            var visitor = new NestingLevelVisitor();
+            var visitor = new IfStatementNestingLevelVisitor();
             var nestingDepth = visitor.Visit(ifStatementSyntax);
 
             if (nestingDepth >= 4)
@@ -46,24 +44,9 @@ namespace CodeAnalyzer
             }
 
         }
-
-        //private static void AnalyzeSymbol(SymbolAnalysisContext context)
-        //{
-        //    // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-        //    var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
-
-        //    // Find just those named type symbols with names containing lowercase letters.
-        //    if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
-        //    {
-        //        // For all such symbols, produce a diagnostic.
-        //        var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-
-        //        context.ReportDiagnostic(diagnostic);
-        //    }
-        //}
     }
 
-    class NestingLevelVisitor : CSharpSyntaxVisitor<int>
+    class IfStatementNestingLevelVisitor : CSharpSyntaxVisitor<int>
     {
         public override int VisitBlock(BlockSyntax node)
         {
